8b593e5f9454de111d61137bb29894cb97cbaf37
Nicola Solazzo <niksolaz@iMac-di-Nicola.local>
2018-09-12 13:52:24 +0200

    Add method to read file txt

--- console/Program.cs
+++ console/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace TeleprompterConsole
 {
@@ -6,7 +8,26 @@ namespace TeleprompterConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var lines = ReadFrom("sampleQuotes.txt");
+            foreach( var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+        static IEnumerable<string> ReadFrom(string file)
+        {
+            string line;
+            using ( var reader = File.OpenText(file))
+            {
+                while((line = reader.ReadLine()) != null)
+                {
+                    yield return line;
+                }
+            }
         }
     }
+
+    
+
 }
